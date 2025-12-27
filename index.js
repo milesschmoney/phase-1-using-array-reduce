533321ae858fd43ee49b521092ea7321e6983535
Alex Ndaula <your_email@example.com>
2025-12-27 15:24:13 -0500

    Complete array reduce lab: totalBatteries

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// DO NOT change this variable name
+const batteryBatches = [
+  { type: "AA", count: 12 },
+  { type: "AAA", count: 24 },
+  { type: "C", count: -5 }
+];
 
-// Code your solution here
+// Use reduce() to get the total number of batteries
+const totalBatteries = batteryBatches.reduce((sum, batch) => {
+  return sum + batch.count;
+}, 0);
+
+module.exports = {
+  totalBatteries
+};
+
